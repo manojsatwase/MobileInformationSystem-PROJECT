797268e93952d4f4a7b644dd0a9aa968320bbc1c
manojsatwase <33016614+manojsatwase@users.noreply.github.com>
2020-02-08 11:11:36 +0530

    Add files via upload

---
+++ mobileData.js
@@ -0,0 +1,78 @@
+var results = {
+  // cardInfos: {
+  //   name: "shopping card",
+  //   card: "debit card credit card"
+  // },
+  cardItems: [
+    {
+      id: 1,
+      name: "iphone x",
+      img:
+        "https://images.pexels.com/photos/2765873/pexels-photo-2765873.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      price: "70,000rs",
+      Colour: "Space Grey",
+      SizeName: " 64GB",
+      description:
+        "Our vision has always been to create an iPhone that is entirely screen. your voice and even a glance. With iPhone X, that vision is now a reality. Say hello to the future."
+    },
+    {
+      id: 2,
+      name: "MI A2",
+      img:
+        "https://images.pexels.com/photos/2756634/pexels-photo-2756634.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      price: "16,000rs",
+      Colour: "Space Grey",
+      SizeName: " 64GB",
+      description:
+        "Our vision has always been to create an iPhone that is entirely screen. your voice and even a glance. With iPhone X, that vision is now a reality. Say hello to the future."
+    },
+    {
+      id: 3,
+      name: "oppo f1",
+      img:
+        "https://images.pexels.com/photos/47261/pexels-photo-47261.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+
+      price: "70,0000rs",
+      Colour: "Space Grey",
+      SizeName: " 64GB",
+      description:
+        "Our vision has always been to create an iPhone that is entirely screen. your voice and even a glance. With iPhone X, that vision is now a reality. Say hello to the future."
+    },
+    {
+      id: 4,
+      name: "oppo f1",
+      img:
+        "https://images.pexels.com/photos/2756634/pexels-photo-2756634.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      name: "oppo f2",
+      price: "30,0000rs",
+      Colour: "balck",
+      SizeName: " 64GB",
+      description:
+        "Our vision has always been to create an iPhone that is entirely screen. your voice and even a glance. With iPhone X, that vision is now a reality. Say hello to the future."
+    },
+    {
+      id: 5,
+      name: "oppo f2",
+      img:
+        "https://images.pexels.com/photos/47261/pexels-photo-47261.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+
+      price: "40,0000rs",
+      Colour: "sky blue",
+      SizeName: " 64GB",
+      description:
+        "Our vision has always been to create an iPhone that is entirely screen. your voice and even a glance. With iPhone X, that vision is now a reality. Say hello to the future."
+    },
+    {
+      id: 6,
+      name: "oppo f1",
+      img:
+        "https://images.pexels.com/photos/248526/pexels-photo-248526.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      price: "70,0000rs",
+      Colour: "Space Grey",
+      SizeName: " 64GB",
+      description:
+        "Our vision has always been to create an iPhone that is entirely screen. your voice and even a glance. With iPhone X, that vision is now a reality. Say hello to the future."
+    }
+  ]
+};
+module.exports = results;
